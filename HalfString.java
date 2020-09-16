bccd5870fed897668d994ded400f946f76ee946c
guidoasbun <gdrodrigo8@gmail.com>
2020-09-16 10:42:18 -0700

    Part A complete

---
+++ HalfString.java
@@ -0,0 +1,24 @@
+/* 
+*   Guido Asbun
+*   CS A170 
+*   15-September-2020     
+* 
+*   Exercise 05
+*/ 
+
+import java.util.Scanner;
+public class HalfString{
+    public static void main(String [] args){
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Please enter an EVEN length string: ");
+        
+        String inputString = input.nextLine();
+        int stringLength = inputString.length();
+        int halfString = stringLength / 2;
+        String newString = inputString.substring(0,halfString);
+;        
+        System.out.println(newString);
+      
+    }
+}
